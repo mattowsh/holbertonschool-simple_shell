0f2cdae0427225eeafd013b38d0a5c95add7ef13
gallit0 <mateogesede@gmail.com>
2022-07-26 10:44:11 -0700

    still a lot to do :p

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+char **str_to_argv(char *input, )
+{
+	char **result;
+
+	malloc
+}
+
+
 int non_interactive(char *p1, char *argv1, char *argv2, char *argv3)
 {
 	char *full_path;
@@ -25,6 +33,30 @@ int non_interactive(char *p1, char *argv1, char *argv2, char *argv3)
 	return (-1);
 }
 
+int interactive(char *b, char *p1)
+{
+	char *full_path;
+	int file;
+	int i = 0;
+
+	p1 = strtok(p1, "-");
+	while (p1)
+	{
+		if (!(full_path = malloc(1024)))
+			return (-1);
+		strcat(full_path, p1);
+		strcat(full_path, "/");
+		if ((file = open(full_path, O_RDONLY)) == 3)
+		{
+			close(file);
+			execve(full_path, b, NULL);
+		}
+		p1 = strtok(NULL, ":");
+		free(full_path);
+	}
+	return (-1);
+}
+
 int main(int ac, char **av, char **env)
 {
 	size_t bufsize = 1024;
@@ -32,6 +64,8 @@ int main(int ac, char **av, char **env)
 	int characters;
 	char *p = getenv("PATH");
 	char *p1 = strdup(p);
+	int fr;
+	int status, pid;
 
 	if (ac > 1)
 		return (non_interactive(p1, av[1], av[2], av[3]));
@@ -42,7 +76,19 @@ int main(int ac, char **av, char **env)
 			return (-1);
 		printf("#cisfun$ ");
 		characters = getline(&b, &bufsize, stdin);
+		
+		switch (pid = fork())
+		{
+			case -1:
+				exit(-1);
+				break;
+			case 0: /*Proceso hijo*/
 
+				break;
+			default: /*Proceso padre*/
+				wait(&status);
+				break;
+		}
 		free(b);
 	}
 	return (0);
