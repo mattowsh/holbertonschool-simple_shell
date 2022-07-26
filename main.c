05e86bfc9a0e8c18fa0542eb39d452e2724507fe
gallit0 <mateogesede@gmail.com>
2022-07-25 17:02:35 -0700

    non-interactive mode kinda finished

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include "main.h"
 
-int main(void)
+int main(int ac, char **av, char **env)
 {
-	return (0):
+	size_t bufsize = 256;
+	char buffer[bufsize];
+	char *b = buffer;
+	int characters;
+	char *p = getenv("PATH");
+	char *p1 = strdup(p);
+	int file;
+	char *full_path;
+	if (ac > 1)
+	{
+		p1 = strtok(p1, ":");
+		while (p1)
+		{
+			if (!(full_path = malloc(1024)))
+				return(0);
+			strcat(full_path, p1);
+			strcat(full_path, "/");
+			strcat(full_path, av[1]);
+			if ((file = open(full_path, O_RDONLY)) == 3)
+			{
+				char *argv[] = { full_path, av[2] };
+				execve(full_path, argv, NULL);
+			}
+			close(file);
+			p1 = strtok(NULL, ":");
+			free(full_path);
+		}
+		return (0);
+	}
+
+	while(1)
+	{
+		printf("($) ");
+		characters = getline(&b, &bufsize, stdin);
+	}
+	return (0);
 }
--- main.h
+++ main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #endif
