45178103c88c4a98b0e905c132bbc7404d3112ce
gallit0 <mateogesede@gmail.com>
2022-07-28 08:58:54 -0700

    set_strtok function finished

--- set_strtok.c
+++ set_strtok.c
@@ -2,46 +2,47 @@
 
 
 
-char ***set_strtok(char *input, char *sep)
+char **set_strtok(char *input, char *sep)
 {
 	char *in = input;
 	char *in1 = strdup(in);
-	char *temp = strdup(in);
-	char ***result;
+	char *in2 = strdup(in);
+	char **result;
 	int i, len, j = 0;
 
-	in1 = strtok(in1, sep);
-	while (in1)
+	in2 = strtok(in2, sep);
+	while (in2)
 	{
 		j++;
-		in1 = strtok(NULL, sep);
+		in2 = strtok(NULL, sep);
 	}
 	if (j == 0)
 		return (0);
-
 	in1 = strdup(in);
 
-	(*result) = malloc(sizeof(char *) * j);
-	if (!(*result))
+	result = malloc(sizeof(char *) * (j + 1));
+	if (!(result))
 		return (0);
 
+	printf("first success\n");
 	in1 = strtok(in1, sep);
-	temp = strtok(in1, sep);
-	for (i = 0; (*result)[i]; i++)
+	for (i = 0; i < j; i++)
 	{
-		if ((temp = strtok(NULL, sep)) == NULL)
-			in1 = strtok(in1, "\n");
+		printf("second success\n");
 		len = strlen(in1);
-		(*result)[i] = malloc(len);
-		if (!(*result)[i])
+		result[i] = malloc(len++);
+		printf("third success");
+		if (!(result[i]))
 		{
+			printf("error");
 			for (; i >= 0; i--)
-				free((*result)[i]);
-			free(*result);	
+				free(result[i]);
+			free(result);
 		}
-		(*result)[i] = strdup(in1);
+		printf("fourth success\n");
+		result[i] = strdup(in1);
 		in1 = strtok(NULL, sep);
 	}
-
+	result[++i] = 0;
 	return (result);
 }
