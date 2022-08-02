5c9a9f67a9fcfd9eca98065ac207984616b334c1
mattowsh <5089@holbertonschool.com>
2022-08-02 08:00:28 -0700

    betty style ok

--- _getenv.c
+++ _getenv.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _getenv - 
- * @env : 
+ * _getenv - Searchs the PATH environment variable on
+ * the environment
+ * @e : the  environment list
  *
- * Return:
+ * Return: the value of PATH in string format
  */
 
 char *_getenv(char **e)
 {
-	char **env = e;
-	char *p, *result;
+	char **env = e, *p, *result;
 	int i;
-
 
 	for (i = 0; env[i]; i++)
 	{
